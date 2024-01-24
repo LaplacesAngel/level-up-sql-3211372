39f4bd9daac63fdfa1aae8626cb9f87eb7d3fa19
paul <paulentz@gmail.com>
2024-01-24 13:17:31 -0600

    first challenge done

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,11 @@
 -- Generate a list of customer information.
+select * from customers;
 
 -- Show their first name, last name, and email address.
+select FirstName,  LastName,  Email from customers;
+
 -- Sort the list of results by last name.
+
+select FirstName,  LastName, Email 
+from customers 
+order by LastName;
