3003bc26dbbb2cd14a178385c74b841dad3562ea
paul <paulentz@gmail.com>
2024-03-30 20:35:44 -0500

    refreshing sql

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,33 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+select * 
+FROM Customers
+where FirstName = 'Cleo';
+
+.tables
+
+.schema Dishes
+.schema Customers
+
+select DishID
+FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad';
+
+UPDATE Customers
+SET FavoriteDish = (select DishID
+                      FROM Dishes
+                      WHERE Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = (select CustomerID 
+                    FROM Customers
+                    where FirstName = 'Cleo');
+
+
+SELECT Customers.FirstName, Customers.LastName, Customers.FavoriteDish, Dishes.Name
+FROM Customers
+JOIN Dishes
+ON Customers.FavoriteDish = Dishes.DishID;
+WHERE Customers.CustomerID = (select CustomerID 
+                    FROM Customers
+                    where FirstName = 'Cleo');
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,40 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+.tables
+
+.schema
+
+
+-- will need the customer name and email from the customers table. 
+-- will need info from the orders table with the cust id, prob just group by cust id and count order ids
+
+select DISTINCT Customers.FirstName, Customers.LastName, Customers.Email, count(Orders.OrderID) AS Count
+from Customers
+join Orders on Orders.CustomerID = Customers.CustomerID
+group by Orders.CustomerID
+order by Count DESC
+limit 6;
+
+
+
+
+
+
+
+
+
+
+-- .tables
+
+-- .schema Orders
+-- .schema CustomersDishes
+
+-- select Customers.CustomerID, Customers.FirstName, Customers.LastName, Customers.Email, count(Orders.OrderID) as Count
+-- FROM Orders
+-- JOIN Customers
+-- ON Orders.CustomerID = Customers.CustomerID
+-- GROUP BY Orders.CustomerID
+-- ORDER BY Count DESC
+-- LIMIT 5;
