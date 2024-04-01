54c6a4155167e57a7b07360c71c41d8b26d290b4
paul <paulentz@gmail.com>
2024-04-01 16:06:44 -0500

    challenge 1 done

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,10 +1,16 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
 
--- .tables
 
--- .schema
-
-select Title, count(Title) AS count
-from Books
-where Title = 'Dracula';
+select(
+select count(*)
+From Books
+where Books.Title = 'Dracula')
+-
+(select count(*)
+from loans
+join Books on Books.BookID = Loans.LoanID
+where Books.Title = 'Dracula' 
+AND  Loans.ReturnedDate IS NULL)
+as Copies_Available
+;
